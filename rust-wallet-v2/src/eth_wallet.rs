9585fda9f12b5f7f691be59d618137c75c754a55
zander-raycraft <zanderraycraft504@gmail.com>
2023-06-02 22:21:28 -0500

    Seventh Commit

--- rust-wallet-v2/src/eth_wallet.rs
+++ rust-wallet-v2/src/eth_wallet.rs
@@ -1,22 +1,30 @@
-//Making a wallet on the ethereum network
-
 /*
+PROJECT SPECS:
 * 1) Make function for generating a key pair (public/private)
-* 2)
+* 2) Take the Private Key and convert to Hex address and store/save it
+* 3)
 */
 
+//Running for error handeling
+use anyhow::{bail, Result};
 
 use secp256k1::{
     rand::{rngs, SeedableRng},
     PublicKey, SecretKey,
 };
 
+//Scoping in new objects for reading and writing files
+use serde::{Deserialize, Serialize};
+use std::io::BufWriter;
+use std::str::FromStr;
+use std::{fs::OpenOptions, io::BufReader};
+
 //function for getting the users address using the keccak packages allowing for transfers
 use tiny_keccak::keccak256;
-use web3::types::Address;
+use web3::{types::Address};
 
 /*
- Function: generate_keypar
+ Function: generate_keypair
  Purpose: returns the address of the users publicKey
  @Params: PublicKey -> the useres public key
  @returns: Address -> the address of the users wallet
@@ -38,7 +46,7 @@ pub fn generate_keypair() -> (SecretKey, PublicKey) {
         serialized publicKey
  */
 
-pub fn return_public_address(public_key: &PublicKey) -> Address {
+pub fn public_key_address(public_key: &PublicKey) -> Address {
     let public_key= public_key.serialize_uncompressed();
     //check if serialize returns the correct thing
     debug_assert_eq!(public_key[0], 0x04);
@@ -46,3 +54,55 @@ pub fn return_public_address(public_key: &PublicKey) -> Address {
 
     Address::from_slice(&hash[12..]) //setting value of return var
 }
+
+//Making the wallet structure
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Wallet {
+    //Members of class wallet
+    //Public
+    pub secret_key: String,
+    pub public_key: String,
+    pub public_address: String,
+    //Private
+}
+
+
+impl Wallet {
+    /*
+    Function: new
+    Purpose: Default Ctor for the wallet object, saving data to file,
+            and loading data from file (wallet)
+    @Params: secret_key -> Uses secret key;
+             public_key -> Users public key
+    @returns: Self -> constructed wallet (this)
+    @notes: N/A
+     */
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let addr: Address = public_key_address(&public_key);
+        Wallet {
+            secret_key: secret_key.to_string(),
+            public_key: public_key.to_string(),
+            public_address: format!("{:?}", addr), //Formatting the address variable
+        }
+    }
+    /*
+    Function: save_as_file
+    Purpose: saves the data from the wallet to a disk
+    @Params: &self -> the wallet itslef
+            file_path -> string for where the resulting file is going to go
+    @returns: Disk<()> -> disk containing the information we need
+    @notes: We are using hashing and the keccak256 package to get the actual address from the
+            serialized publicKey
+    */
+    pub fn save_as_file(&self, file_path: &str) -> Result<()> {
+        //Using Standard Library to create new object
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(file_path)?;
+        let buf_writer = BufWriter::new(file);
+        serde_json::to_writer_pretty(buf_writer, self)?;
+        Ok(())
+    }
+}
+
--- rust-wallet-v2/src/main.rs
+++ rust-wallet-v2/src/main.rs
@@ -1,9 +1,18 @@
+use anyhow::Result;
 mod eth_wallet;
-fn main() {
+fn main() -> Result<()>{
     let (secret_key, pub_key) = eth_wallet::generate_keypair();
     println!("secret key: {}", &secret_key.to_string());
     println!("public key: {}", &pub_key.to_string());
 
-    let pub_address = eth_wallet::return_public_address(&pub_key);
+    let pub_address = eth_wallet::public_key_address(&pub_key);
     println!("public address: {:?}", pub_address);
+
+    let crypto_wallet = eth_wallet::Wallet::new(&secret_key, &pub_key);
+    println!("crypto_wallet: {:?}", &crypto_wallet);
+
+    //saving info to a file
+    crypto_wallet.save_as_file("crypto_wallet.json")?;
+
+    Ok(())
 }
